3605da76ac75a442a4b9215836c64120be8c52eb
Ismia Kariny <ismia.kariny@gmail.com>
2022-06-06 14:30:22 -0300

    Adicionando funcionalidade ao botão para adicionar uma nova palavra ao jogo

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 var telaInicial = document.querySelector('#tela-inicial');
 var botaoComecar = document.querySelector('.botao-comecar');
+var botaoAdicionar = document.querySelector('.botao-adicionar');
 var telaDoJogo = document.querySelector('#tela-do-jogo');
+var telaAdicionar = document.querySelector('#tela-adicionar')
 
 botaoComecar.addEventListener('click', function(){
     if ($('#tela-inicial').not('.invisivel').length) {
@@ -13,4 +15,17 @@ botaoComecar.addEventListener('click', function(){
     }
 
 });
+
+botaoAdicionar.addEventListener('click', function() {
+    if ($('#tela-inicial').not('.invisivel').length) {
+        telaInicial.classList.add('invisivel');        
+    }
+
+    if($('#tela-adicionar').hasClass('invisivel')) {
+        telaAdicionar.classList.remove('invisivel');
+        alert("Adicione uma nova palavra!");
+    }
+})
+
+
 
